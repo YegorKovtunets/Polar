7b5f6f798e3ab235f0d949518e80ff166b53fa7f
Yegor Kovtunets <119995830+YegorKovtunets@users.noreply.github.com>
2022-12-07 10:58:41 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function () {
+    $(".standart_btn").click(function () {
+        $(".modal_standart_btn_block").toggleClass("hidden");
+    });
+    $(".modal_close").click(function () {
+        $(".modal_standart_btn_block").toggleClass("hidden");
+    });
+});
